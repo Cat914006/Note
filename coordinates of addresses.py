547638d5a6fc0e34b0c2062db117e88113530d80
Cat <95224787+Cat914006@users.noreply.github.com>
2023-07-31 13:41:24 +0800

    精簡

--- coordinates of addresses.py
+++ coordinates of addresses.py
@@ -4,12 +4,6 @@
 
 # 全聯地址經緯度
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import os
 import pandas as pd
